612d2688c758b45d7ac25f111e95f703070b8c68
Rakesh <rak.dontmess@gmail.com>
2014-03-08 23:17:36 -0500

    port to 2.7 for boto

--- services/gmaps/distancematrix.py
+++ services/gmaps/distancematrix.py
@@ -1,18 +1,13 @@
-import urllib.parse
-import urllib.request
+import urllib
 import json
 
-DISTANCE_BASE_URL = "http://maps.googleapis.com/maps/api/distancematrix/json?"
+DISTANCE_BASE_URL = "http://maps.googleapis.com/maps/api/distancematrix/json?%s"
 
 def distance(origin, destination):
     url = construct_distance_url(origin, destination)
-    data = urllib.request.urlopen(url)
-    data = json.loads(data.read().decode("utf-8"))
+    data = json.loads(url.read().decode("utf-8"))
     return data["rows"][0]["elements"][0]["distance"]["text"]
     
 def construct_distance_url(origin, destination):
-    url = []
-    url.append(DISTANCE_BASE_URL)
-    params = { "origins" : origin, "destinations" : destination, "sensor" : "false"}
-    url.append(urllib.parse.urlencode(params))
-    return ''.join(url)
+    params = urllib.urlencode({ "origins" : origin, "destinations" : destination, "sensor" : "false"})
+    return urllib.urlopen(DISTANCE_BASE_URL % params)
